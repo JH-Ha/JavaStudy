88b0d2b2769a555dc80e13f4ca1ed46b42e1fdbc
Jaehyeong Ha <cubeprince@gmail.com>
2021-06-05 22:29:06 +0900

    add discountCondition test

--- src/object/ch2/TestMain.java
+++ src/object/ch2/TestMain.java
@@ -8,7 +8,7 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Main {
+public class TestMain {
 	@Test
 	public void checkAmountDiscountPolicy() {
 		Movie avatar = new Movie("아바타", Duration.ofMinutes(120), Money.wons(10000),
@@ -17,5 +17,15 @@ public class Main {
 		Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 6, 7, 10, 30));
 
 		Assertions.assertEquals(Money.wons(9200.0).getAmount(), (screening.calculateFee(1).getAmount()));
+	}
+
+	@Test
+	public void checkAmountDiscountPolicyNotExpectedCondition() {
+		Movie avatar = new Movie("아바타", Duration.ofMinutes(120), Money.wons(10000),
+				new AmountDiscountPolicy(Money.wons(800), new SequenceCondition(1), new SequenceCondition(10),
+						new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
+		Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 6, 8, 10, 30));
+
+		Assertions.assertEquals(Money.wons(10000.0).getAmount(), (screening.calculateFee(1).getAmount()));
 	}
 }
