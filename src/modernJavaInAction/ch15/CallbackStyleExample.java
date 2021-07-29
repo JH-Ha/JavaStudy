fd23d679a53cf868af24a5db47d16bbd06210faf
Jaehyeong Ha <cubeprince@gmail.com>
2021-07-30 00:47:46 +0900

    15.2.2 callback style example

---
+++ src/modernJavaInAction/ch15/CallbackStyleExample.java
@@ -0,0 +1,36 @@
+package modernJavaInAction.ch15;
+
+import java.util.function.IntConsumer;
+
+public class CallbackStyleExample {
+
+	public static void f(int x, IntConsumer dealWithResult) {
+		dealWithResult.accept(x);
+	}
+
+	public static void g(int x, IntConsumer dealWithResult) {
+		dealWithResult.accept(x);
+	}
+
+	public static void main(String[] args) {
+		int x = 1337;
+		Result result = new Result();
+
+		f(x, (int y) -> {
+			result.left = y;
+			System.out.println(result.left + result.right);
+		});
+
+		g(x, (int z) -> {
+			result.right = z;
+			System.out.println(result.left + result.right);
+		});
+
+	}
+}
+
+class Result {
+
+	int left;
+	int right;
+}
