8d76d521089766d62f4f30b6c7fcf92c8883b5e6
Jaehyeong Ha <cubeprince@gmail.com>
2021-08-03 02:41:16 +0900

    chapter 17 Subscriber and Publisher

---
+++ src/modernJavaInAction/ch17/TempSubscription.java
@@ -0,0 +1,38 @@
+package modernJavaInAction.ch17;
+
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class TempSubscription implements Subscription {
+
+	private final Subscriber<? super TempInfo> subscriber;
+	private final String town;
+
+	public TempSubscription(Subscriber<? super TempInfo> subscriber, String town) {
+		super();
+		this.subscriber = subscriber;
+		this.town = town;
+	}
+
+	@Override
+	public void request(long n) {
+		// TODO Auto-generated method stub
+		for (long i = 0L; i < n; i++) {
+
+			try {
+				subscriber.onNext(TempInfo.fetch(town));
+			} catch (Exception e) {
+				subscriber.onError(e);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+		subscriber.onComplete();
+
+	}
+
+}
