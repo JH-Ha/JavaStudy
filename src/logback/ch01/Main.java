b804d5f41ae422c51e4a023abf75bc557208d93f
Jaehyeong Ha <cubeprince@gmail.com>
2021-08-09 21:57:44 +0900

    add log dependencies

--- src/logback/ch01/Main.java
+++ src/logback/ch01/Main.java
@@ -1,7 +1,11 @@
 package logback.ch01;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 	public static void main(String[] args) {
-
+		Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1");
+		logger.debug("Hello world.");
 	}
 }
--- src/object/ch2/TestMain.java
+++ src/object/ch2/TestMain.java
@@ -1,39 +1,33 @@
 package object.ch2;
 
-import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class TestMain {
-	@Test
-	public void checkAmountDiscountPolicy() {
-		Movie avatar = new Movie("아바타", Duration.ofMinutes(120), Money.wons(10000),
-				new AmountDiscountPolicy(Money.wons(800), new SequenceCondition(1), new SequenceCondition(10),
-						new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
-		Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 6, 7, 10, 30));
-
-		Assertions.assertEquals(Money.wons(9200.0).getAmount(), (screening.calculateFee(1).getAmount()));
-	}
-
-	@Test
-	public void checkAmountDiscountPolicyNotExpectedCondition() {
-		Movie avatar = new Movie("아바타", Duration.ofMinutes(120), Money.wons(10000),
-				new AmountDiscountPolicy(Money.wons(800), new SequenceCondition(1), new SequenceCondition(10),
-						new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
-		Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 6, 8, 10, 30));
-
-		Assertions.assertEquals(Money.wons(10000.0).getAmount(), (screening.calculateFee(1).getAmount()));
-	}
-
-	@Test
-	public void checkNoneDiscountPolicy() {
-		Movie avatar = new Movie("스타워즈", Duration.ofMinutes(210), Money.wons(10000), new NoneDiscountPolicy());
-		Screening screening = new Screening(avatar, 2, LocalDateTime.of(2021, 6, 8, 10, 30));
-
-		Assertions.assertEquals(Money.wons(10000.0).getAmount(), (screening.calculateFee(1).getAmount()));
-	}
+	/*
+	 * @Test public void checkAmountDiscountPolicy() { Movie avatar = new
+	 * Movie("아바타", Duration.ofMinutes(120), Money.wons(10000), new
+	 * AmountDiscountPolicy(Money.wons(800), new SequenceCondition(1), new
+	 * SequenceCondition(10), new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10,
+	 * 0), LocalTime.of(11, 59)))); Screening screening = new Screening(avatar, 2,
+	 * LocalDateTime.of(2021, 6, 7, 10, 30));
+	 * 
+	 * Assertions.assertEquals(Money.wons(9200.0).getAmount(),
+	 * (screening.calculateFee(1).getAmount())); }
+	 * 
+	 * @Test public void checkAmountDiscountPolicyNotExpectedCondition() { Movie
+	 * avatar = new Movie("아바타", Duration.ofMinutes(120), Money.wons(10000), new
+	 * AmountDiscountPolicy(Money.wons(800), new SequenceCondition(1), new
+	 * SequenceCondition(10), new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10,
+	 * 0), LocalTime.of(11, 59)))); Screening screening = new Screening(avatar, 2,
+	 * LocalDateTime.of(2021, 6, 8, 10, 30));
+	 * 
+	 * Assertions.assertEquals(Money.wons(10000.0).getAmount(),
+	 * (screening.calculateFee(1).getAmount())); }
+	 * 
+	 * @Test public void checkNoneDiscountPolicy() { Movie avatar = new
+	 * Movie("스타워즈", Duration.ofMinutes(210), Money.wons(10000), new
+	 * NoneDiscountPolicy()); Screening screening = new Screening(avatar, 2,
+	 * LocalDateTime.of(2021, 6, 8, 10, 30));
+	 * 
+	 * Assertions.assertEquals(Money.wons(10000.0).getAmount(),
+	 * (screening.calculateFee(1).getAmount())); }
+	 */
 }
